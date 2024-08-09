9219b632b2abec1a711219a24efeb5e81bb6a30c
vick-ram <vickramodero6@gmail.com>
2024-08-09 08:17:35 -0700

    file-6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,7 +1,7 @@
 -- Script creates stored procedure addBonus that adds
 -- a new correction for a student
-DELIMITER |
-CREATE PROCEDURE addBonus (IN user_id, project_name, score)
+DELIMITER $$
+CREATE PROCEDURE addBonus (IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
 	DECLARE project_id INT;
 	SELECT id INTO project_id
@@ -12,6 +12,5 @@ BEGIN
 		SET project_id = LAST_INSERT_ID();
 	END IF;
 	INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
-END;
-|
-DELIMITER;
+END $$
+DELIMITER ;
