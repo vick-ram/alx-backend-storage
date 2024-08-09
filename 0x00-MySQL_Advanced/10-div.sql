6b840c8272174fb9f86b9d49506f66ea0189d855
vick-ram <vickramodero6@gmail.com>
2024-08-09 11:10:35 -0700

    file-10 correction

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -2,7 +2,8 @@
 -- the second number or returns 0 if the second number is equal to 0
 DELIMITER $$
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS DECIMAL(10, 8)
+RETURNS FLOAT
+DETERMINISTIC
 BEGIN
 	IF b = 0 THEN
 		RETURN 0;
