db6c04a6a4364e2c83ae1516161e222489085a0a
vick-ram <vickramodero6@gmail.com>
2024-08-09 11:48:50 -0700

    file-100

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -6,20 +6,18 @@ BEGIN
 	DECLARE weighted_sum FLOAT DEFAULT 0;
 	DECLARE total_weight INT DEFAULT 0;
 
-	SELECT SUM(c.score * p.weight)
-	INTO weighted_sum,
-	SUM(p.weight) INTO total_weight
+	SELECT SUM(c.score * p.weight) INTO weighted_sum
 	FROM corrections c
 	JOIN projects p ON c.project_id = p.id
 	WHERE c.user_id = user_id;
 
+	SELECT SUM(p.weight) INTO total_weight
+	FROM projects p
+	WHERE p.id IN (SELECT project_id FROM corections WHERE user_id = user_id);
+
 	IF total_weight > 0 THEN
 		UPDATE users
 		SET average_score = weighted_sum / total_weight
-		WHERE id = user_id;
-	ELSE
-		UPDATE users
-		SET average_score = 0
 		WHERE id = user_id;
 	END IF;
 END $$
