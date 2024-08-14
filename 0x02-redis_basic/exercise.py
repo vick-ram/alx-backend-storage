38064c887811b054ea75ff8a2b671c2d92fee532
vick-ram <vickramodero6@gmail.com>
2024-08-14 09:44:51 -0700

    more functions

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,3 +16,18 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+
+    def get(self, key: str, fn: callable = None) -> Union[str, bytes, int, float, None]:
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        return fn(value) if fn else value
+
+
+    def get_str(self, key: str) -> str:
+        return self.get(key, fn=lambda d: d.decoode("utf-8"))
+
+
+    def get_int(self, key: str) -> int:
+        return self.get(key, fn=int)
