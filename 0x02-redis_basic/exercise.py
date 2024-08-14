b4d075b49d9ce94ed6fbf9c389cf70acd4364054
vick-ram <vickramodero6@gmail.com>
2024-08-14 10:04:21 -0700

    added decorator function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """exercise module"""
 import redis
 import uuid
+import functools
 from typing import Union
 
 
@@ -31,3 +32,19 @@ class Cache:
 
     def get_int(self, key: str) -> int:
         return self.get(key, fn=int)
+
+
+    def count_calls(self, func):
+        @functools.wraps(func)
+        def wrapper(self, *args, **kwargs):
+            key = func.__qualname__
+            self._redis.incr(key)
+            return func(self, *args, **kwargs)
+        return wrapper
+
+
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
