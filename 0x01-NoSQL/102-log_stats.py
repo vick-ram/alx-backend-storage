edd58829867eed4d6f4314648f9f5dcda4819662
vick-ram <vickramodero6@gmail.com>
2024-08-16 14:22:58 -0700

    final correction

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -23,7 +23,8 @@ def log_stats():
 
     """Count number of GET requests to /status"""
     status_check_count = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"}: )
+        {"method": "GET", "path": "/status"})
+
     print(f"{status_check_count} status check")
     """Aggregate to find the top 10 most present IPs"""
     print("IPs:")
