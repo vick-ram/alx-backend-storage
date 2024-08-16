5497aa868c2b3e11065a14959e9f707b325cdbaf
vick-ram <vickramodero6@gmail.com>
2024-08-16 10:03:51 -0700

    file-11 correction

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,5 +4,5 @@
 
 def schools_by_topic(mongo_collection, topic):
     """Script that returns the list of school having a specific topic"""
-    result = mongo_collection.find({'topic': topic})
+    result = mongo_collection.find({'topics': topic})
     return result
